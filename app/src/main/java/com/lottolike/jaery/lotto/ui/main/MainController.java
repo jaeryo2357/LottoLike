37c46d1712576e3d6e7acdb90b63e7f640470c88
jaeryo2357 <jaeryo2357@naver.com>
2020-06-28 18:39:45 +0900

    onClick 재정의와 observe 인터페이스

--- app/src/main/java/com/lottolike/jaery/lotto/ui/main/MainController.java
+++ app/src/main/java/com/lottolike/jaery/lotto/ui/main/MainController.java
@@ -5,13 +5,20 @@ package com.lottolike.jaery.lotto.ui.main;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
 
 import com.lottolike.jaery.lotto.R;
 import com.lottolike.jaery.lotto.barcode.BarcodeCaptureActivity;
+import com.lottolike.jaery.lotto.ui.GetNumberActivity;
 import com.lottolike.jaery.lotto.ui.MyListActivity;
+import com.lottolike.jaery.lotto.ui.SettingActivity;
+import com.lottolike.jaery.lotto.ui.detail.LottoDetailActivity;
 
 public class MainController extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,20 +36,45 @@ public class MainController extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View view) {
+        Intent intent;
         switch (view.getId()) {
             case R.id.main_qrCode :
-                moveActivity(BarcodeCaptureActivity.class);
+                intent = new Intent(MainController.this, BarcodeCaptureActivity.class);
+                startActivity(intent);
                 break;
 
             case R.id. main_myList:
-                moveActivity(MyListActivity.class);
+                intent = new Intent(MainController.this, MyListActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.main_setting:
+                intent = new Intent(MainController.this, SettingActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.main_get_random_number:
+                intent = new Intent(MainController.this, GetNumberActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.main_info_image_btn:
+
+                TextView today_LottoNumber = findViewById(R.id.lottoResult_title);
+                RelativeLayout today_LottoLayout = findViewById(R.id.main_result_layout);
+
+                intent = new Intent(MainController.this, LottoDetailActivity.class);
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        MainController.this,
+                        new Pair<View,String>(today_LottoNumber,"lottoTitle"),
+                        new Pair<View,String>(today_LottoLayout,"lottoNumber"));
+                startActivity(intent,options.toBundle());
                 break;
         }
     }
 
-    //인텐트 이동이 많아 별도의 함수로 생성 Class의 매개변수가 안전한 방식인지 확인 필요
-    private void moveActivity(Class move) {
-        Intent intent = new Intent(MainController.this, move);
-        startActivity(intent);
+    //모델의 변수 변화를 구독 후, 변경되면 View 반영
+    private void observeModel() {
+
     }
 }
